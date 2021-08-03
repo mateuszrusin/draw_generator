11efc23da847694229d5fda57b2f3b24db31b996
Mateusz Rusin <mateusz.rusin@pearson.com>
2021-08-03 21:00:54 +0200

    Some material-ui sweets

--- dg/src/App.js
+++ dg/src/App.js
@@ -6,7 +6,6 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import Divider from "@material-ui/core/Divider";
 import users from "./users";
 import ListItemAvatar from "@material-ui/core/ListItemAvatar";
 import Avatar from "@material-ui/core/Avatar";
@@ -14,13 +13,34 @@ import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 import Checkbox from "@material-ui/core/Checkbox";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
+import {Box} from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
 
 function App() {
 
     const username = (id) => users.find(user => user.id === id).name;
-    const draw = drawGenerator(100);
-    const data = draw[1];
+    const roundName = (round) => {
+        if (rounds - round === 0) {
+            return 'FINAL'
+        }
+
+        return '1/' + Math.pow(2, rounds - round)
+    };
+
+    const draw = drawGenerator(16);
     const rounds = Object.keys(draw).length;
+
+    const Game = ({game}) => (
+        <Box m={3}>
+            <Paper elevation={3}>
+                <List component="nav" aria-label="main mailbox folders">
+                    <Player id={game.seed} />
+                    <Player id={game.opponent} />
+                </List>
+            </Paper>
+        </Box>
+    )
+
     const Player = ({id}) => (
         <ListItem button>
             <ListItemAvatar>
@@ -34,32 +54,31 @@ function App() {
             </ListItemIcon>
             <ListItemText primary={username(id)} />
             <ListItemSecondaryAction>
-                <Checkbox
-                    edge="end"
-                />
+                <Checkbox edge="end" />
             </ListItemSecondaryAction>
         </ListItem>
     );
 
+    const Phase = ({round}) => (
+        <Typography variant="h5" component="h5">
+            {roundName(round)}
+        </Typography>
+    );
+
+    const Round = ({draw, round}) => (
+        <Grid item xs>
+            <Phase round={round} />
+            {draw[round].map((game) => (
+                <Game game={game} />
+            ))}
+        </Grid>
+    );
+
     return (
         <div className="App">
-
-            <Grid container spacing={3}>
-                <Grid item xs>
-                    <Paper>
-                        {data.map((game, index) => (
-                            <List component="nav" aria-label="main mailbox folders" key={index}>
-                                <Player id={game.seed} />
-                                <Player id={game.opponent} />
-                                <Divider />
-                            </List>
-                        ))}
-                    </Paper>
-                </Grid>
-                {[...Array(rounds)].map((elementInArray, index) => (
-                    <Grid item xs id={index}>
-                        <Paper>xs</Paper>
-                    </Grid>
+            <Grid container direction="row" justifyContent="center" alignItems="center">
+                {Object.keys(draw).map((round, index) => (
+                    <Round draw={draw} round={round} />
                 ))}
             </Grid>
         </div>
--- dg/src/drawGenerator.js
+++ dg/src/drawGenerator.js
@@ -2,16 +2,13 @@ export function drawGenerator(numberOfPlayers) {
 
     let rounds = {};
 
-    const branch = function (seed, level, limit, round) {
+    const branch = (seed, level, limit, round) => {
 
         const opponent = Math.pow(2, level) - seed + 1;
 
         switch (true) {
             case limit === level + 1:
-                if (!rounds[round]) {
-                    rounds[round] = [];
-                }
-                rounds[round].push({seed, opponent});
+                rounds[round] ? rounds[round].push({seed, opponent}) : rounds[round] = [{seed, opponent}];
                 break;
             case seed % 2 === 1:
                 branch(seed, level + 1, limit, round);
@@ -23,7 +20,7 @@ export function drawGenerator(numberOfPlayers) {
         }
     }
 
-    let limit = Math.ceil(Math.log2(numberOfPlayers) + 1);
+    const limit = Math.ceil(Math.log2(numberOfPlayers) + 1);
 
     for (let round = 1; round < limit; round++) {
         branch(1, 1, limit - round + 1, round);
